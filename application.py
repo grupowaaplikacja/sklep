fa8deb4e04398eca30fa84557f3d375656f27942
Krystian Stanuch <kstanuch@matrix.umcs.pl>
2019-01-11 18:14:28 +0100

    Tworzy sie pusty plik database.db w model, sqlalchemy zrobic by tworylo tam kurwa tabele

--- application.py
+++ application.py
@@ -1,19 +1,21 @@
 from flask import *
 from werkzeug import *
 from flask_sqlalchemy import SQLAlchemy
-from model.user import User
-
+from  model.user import User
+import os
 
 app = Flask(__name__)
 app.secret_key ='bardzosekretnyklucz'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database/alchemydatabse.db'
+project_dir = os.path.dirname(os.path.abspath(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(project_dir,"model/database.db"))
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = '44444', ssl_context=('ssl/cert.pem', 'ssl/key.pem'))
 
-admin = User(email='fsdfsd', password='gag',first_name='gfdgdsf',last_name='gfdgsf', phone='53243',role=1)
 
-db.session.add(admin)
+#admin = User(email='fsdfsd', password='gag',first_name='gfdgdsf',last_name='gfdgsf', phone='53243',role=1)
+#db.session.add(admin)
+#db.session.commit()
 
-User.query.all()
+#User.query.all()
--- model/user.py
+++ model/user.py
@@ -1,6 +1,11 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
-from .. import application
-from application import db
+from flask import *
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(project_dir,"database.db"))
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,13 +16,17 @@ class User(db.Model):
     phone = db.Column(db.String(50))
     role = db.Column(db.Integer)
 
-    def __reprt__(self):
-        return '<User %r>' %self.email
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
     
-    def __init__(self,email,password,first_name,last_name,phone,role=1):
-        super(User,self).__init__(self,id,email,password,first_name,last_name,phone,role)
+    def __init__(self,email,password,first_name,last_name,phone,role):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.role = role
 
-    db.create_all()
 
 
 
